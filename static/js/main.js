300fce8b6281d6b83fe8b45e38d6d9c57c87c7a7
Anthony Schneider <anthonyserious@gmail.com>
2017-04-09 15:56:05 -0400

    Add in some CSS and components.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,54 @@
-function init() {
-  ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-  );
+class Nav extends React.Component {
+  render() {
+    return (
+      <div className="col s12">
+         <div className="card blue-grey darken-1">
+           <div className="card-content white-text">
+            Nav stuff
+           </div>
+         </div>
+      </div>
+    )
+  }
 }
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="col s12 m4">
+         <div className="card blue-grey darken-1">
+           <div className="card-content white-text">
+             <span className="card-title">{this.props.title}</span>
+
+             <div className="card-action">
+               <a href="#">Details</a>
+             </div>
+           </div>
+         </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+        <div className="row">
+          <Card title="Repos"></Card>
+          <Card title="Pipelines"></Card>
+          <Card title="Jobs"></Card>
+        </div>
+      </div>
+    )
+  }
+}
+
+var HelloMessage = React.createClass({
+  render: function () {
+    return <h1>Hello {this.props.message}!</h1>;
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
